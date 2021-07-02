ce3025a0eaa0200f5777206362d8320b0c37850c
shantam8 <dscharmen@aol.com>
2021-07-02 10:24:05 +0200

    done

--- js/main.js
+++ js/main.js
@@ -1,23 +1,27 @@
 let toggleDarkModeBtn = document.querySelector("#toggleDarkModeBtn");
-let btnSwitch = document.querySelector(".switch");
+let body = document.querySelector("body");
+let header = document.querySelector("header");
+let secMain = document.querySelector("#main");
+let secOverview = document.querySelector("#overview");
 
-
-function toggleDarkMode(){
-    if(btnSwitch.classList.contains("darkModeActive")){
-        btnSwitch.classList.remove("darkModeActive");
-
-    }else{
-        btnSwitch.classList.add("darkModeActive");
-    }
-
+function toggleDarkMode() {
+  if (toggleDarkModeBtn.classList.contains("darkModeActive")) {
+    toggleDarkModeBtn.classList.remove("darkModeActive");
+    body.classList.remove("darkThemeBody");
+    header.classList.remove("darkThemeHeader");
+    secMain.classList.remove("darkThemeSecMain");
+    secOverview.classList.remove("darkThemeSecOverview");
+  } else {
+    toggleDarkModeBtn.classList.add("darkModeActive");
+    body.classList.add("darkThemeBody");
+    header.classList.add("darkThemeHeader");
+    secMain.classList.add("darkThemeSecMain");
+    secOverview.classList.add("darkThemeSecOverview");
+  }
 }
 
-
-function init(){
-    toggleDarkModeBtn.addEventListener("click", toggleDarkMode);
-
-
+function init() {
+  toggleDarkModeBtn.addEventListener("click", toggleDarkMode);
 }
-
 
 window.onload = init();
